12b4395763a092550d2e670a4a06381b413ba82b
Hibon Thomas <hibon.thomas974@gmail.com>
2018-07-22 17:36:32 +0900

    PR04 Creating a a landscape

---
+++ PanzerRoyale/Source/PanzerRoyale/PanzerRoyaleGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PanzerRoyaleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PANZERROYALE_API APanzerRoyaleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
